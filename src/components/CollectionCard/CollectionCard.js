d46273f86f22de6ae843f4e95edfb5221ab62735
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-12-22 11:13:33 +0530

    Built Punklist component

--- src/components/CollectionCard/CollectionCard.js
+++ src/components/CollectionCard/CollectionCard.js
@@ -5,14 +5,19 @@ import weth from "../../assets/weth.png";
 const CollectionCard = ({ id, name, traits, image }) => {
   return (
     <div className="collectionCard">
-      <img src={image} alt="" />
+      <img loading="lazy" src={image} alt="" />
       <div className="collectionCard__details">
         <div className="collectionCard__name">
           {name} <div className="collectionCard__id">#{id}</div>
         </div>
 
         <div className="collectionCard__priceContainer">
-          <img src={weth} className="collectionCard__wethImage" alt="" />
+          <img
+            loading="lazy"
+            src={weth}
+            className="collectionCard__wethImage"
+            alt=""
+          />
           <div className="collectionCard__price">{traits[0]?.value}</div>
         </div>
       </div>
